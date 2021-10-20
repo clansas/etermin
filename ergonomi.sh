59667c2023b71e5847afd4b1e37d81ae6f99b456
clansas <89086618+clansas@users.noreply.github.com>
2021-10-20 22:10:26 +0700

    Create ergonomi.sh

---
+++ ergonomi.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+POOL=ergo-eu1.nanopool.org:11111
+WALLET=9fFyTgMs8EwMtpK5dMmUw928GRjqNbaBT6PB3bNMg3DyndbHqvm.afdhalrz
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+cd "$(dirname "$0")"
+
+./lolamin --algo AUTOLYKOS2 --pool $POOL --user $WALLET $@
+while [ $? -eq 42 ]; do
+    sleep 10s
+    ./lolamin --algo AUTOLYKOS2 --pool $POOL --user $WALLET $@
+done
